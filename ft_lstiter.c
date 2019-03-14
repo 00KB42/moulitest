b971b23cf47b9dc8f1925cb8efcef86e682764ba
00KB42 <00KB42@gmail.com>
2019-03-14 16:25:39 -0700

    ok

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -14,12 +14,11 @@
 
 void	ft_lstiter(t_list *lst, void (*f)(t_list *elem))
 {
-    t_list *next;
-    
-    while (lst)
-    {
-        next = (lst)->next;
-        f(lst);
-        lst = next;
-    }
+	while (lst->next != NULL)
+	{
+		f(lst);
+		lst = lst->next;
+	}
+	f(lst);
+	lst = lst->next;
 }
