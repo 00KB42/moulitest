83cccb1816f3f3279a20baaf170865b4b796f8f5
00KB42 <00KB42@gmail.com>
2019-03-14 16:13:27 -0700

    ok

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -14,10 +14,7 @@
 
 void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
 {
-	t_list *ptr;
-
-	ptr = *alst;
-	del((*alst)->content, (*alst)->content_size);
-	free(*alst);
-	*alst = NULL;
+    del((*alst)->content, (*alst)->content_size);
+    free(*alst);
+    *alst = NULL;
 }
