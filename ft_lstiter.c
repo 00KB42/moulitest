9df348d5b36d2aa96e4a8d2dbcee86a3e023ae05
00KB42 <00KB42@gmail.com>
2019-03-17 20:59:28 -0700

    ok

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -12,11 +12,12 @@
 
 #include "libft.h"
 
-void	ft_lstiter(t_list *lst, void (*f)(t_list *elem))
+int	ft_lstiter(t_list *lst, void (*f)(t_list *elem))
 {
     while (lst)
     {
         f(lst);
         lst = lst->next;
     }
+    return (0);
 }
